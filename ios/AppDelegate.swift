9627ae748fca3f4f0855c3e7240ea40eda607e74
trustfall <syn3rgy@live.ca>
2018-07-01 11:32:11 -0600

    sets up swift for iOS build

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  RNSwiftKotlin
+//
+//  Created by Kyle Erickson on 2018-07-01.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  var bridge: RCTBridge!
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+    
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "RNSwiftKotlin", initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
